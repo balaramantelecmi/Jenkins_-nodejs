e762a6b5a1717928ec12db3cd2e88ce47567b75e
balaramantelecmi <balaraman993@gmail.com>
2015-08-31 12:40:58 +0530

    balaraman text

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ var express=require('express'),
 
 
   app.get('/',function(req,res){
-<<<<<<< HEAD
-    res.send('hello world  murugan Pandian');
-=======
-    res.send('hello world  murugan hi this is');
->>>>>>> b94fa175ebb9724217d0b331102588b3f0fd5a4f
-  });
+    res.send('hello world  balaraman ramamoorthy');  });
 
 
 
