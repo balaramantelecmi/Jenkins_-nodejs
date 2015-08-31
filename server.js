379e56a540380108f38036877ac68ac8e0906c52
balaramantelecmi <balaraman993@gmail.com>
2015-08-31 12:56:14 +0530

    balaraman modify

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express=require('express'),
 
 
   app.get('/',function(req,res){
-    res.send('hello world  balaraman ramamoorthy');  });
+    res.send('hello world  balaraman krishna');  });
 
 
 
