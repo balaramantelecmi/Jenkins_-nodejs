6e7bf52bd1f73ad044af6e6d934383c227618c60
balaramantelecmi <balaraman993@gmail.com>
2015-09-08 15:44:35 +0530

    sri

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ var express=require('express'),
 
 
   app.get('/',function(req,res){
+<<<<<<< HEAD
     res.send('murugan ');  });
+=======
+    res.send('hello world  balaraman rajsunder');  });
+>>>>>>> ca2d3fb936c8a0f60fe24a9a61d9d1f0cf5f1ac7
 
 
 
